56a36919bb28f6a3097978aa8f8ac94f1ee18c39
Gustavo <gustavo81988@gmail.com>
2015-11-11 15:29:22 -0430

    Libreria Configuracion

---
+++ application/config/my_config.php
@@ -0,0 +1,5 @@
+<?
+
+$config['item1'] = "Este es mi item 1";
+$config['item2'] = "Este es mi item 2";
+$config['base_url'] = "http://www.google.com";
--- application/views/frontend/calendario.php
+++ application/views/frontend/calendario.php
@@ -1,7 +1,7 @@
 <article class="articulo">
     
-    
     <?= $this->calendar->generate();?>
-    
+    <?//= $this->config->item('base_url','my_config');?>
+    <?//= $this->config->item('item2');?>
     
 </article>
